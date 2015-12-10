2e018fdd355310b5460d06302e88a2a24f372cdf
RamiGranat <raextheassassin@gmail.com>
2015-12-10 15:50:11 +0100

    Lab6

---
+++ Lab6/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Point
+    {
+        private int y ;
+        private int x ;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public static implicit operator Point3D(Point point)
+        {
+            return new Point3D(12, 34, 56);
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float floater = 1.21f;
+            long longa = 20l;
+            char charred = 'a';
+            string unidentifiedFlyingString = "Hej";
+            ulong longer = 1337UL;
+
+            int heltal = 7;
+            int heltal2 = 13;
+
+            Console.WriteLine(heltal ^ heltal2);
+            Console.WriteLine(heltal & heltal2);
+            Console.WriteLine(heltal | heltal2);
+            Console.WriteLine(heltal >> heltal2);
+            Console.WriteLine(heltal << heltal2);
+
+            Point point = new Point(11, 22);
+            Point3D point3D = (Point3D)point;
+            Console.WriteLine(point3D);
+
+            Console.ReadLine();
+        }
+    }
+}
